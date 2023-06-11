febbc269858b0034939d8b22d8533c379a605e3c
RoysManfo <manforoys@gmail.com>
2023-06-11 19:16:40 +0200

    Select gamemode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from 'react';
 import TitleScreen from './componenets/TitleScreen';
 
 import './css/app.css';
 
+
 function App() {
+	const [gamemode, setGamemode] = useState('');
+
+	useEffect(() => {
+		console.log(gamemode);
+	}, [gamemode]);
+
 	return (
 		<>
-			<TitleScreen />
+			<TitleScreen setGamemode={setGamemode}/>
+
 		</>
 	);
 }
--- src/componenets/TitleScreen.js
+++ src/componenets/TitleScreen.js
@@ -3,16 +3,23 @@ import Animation from './Animation';
 import '../css/titlescreen.css'
 
 
-export default function TitleScreen(){
+export default function TitleScreen(props){
     const [hiddenClass, setHiddenClass] = useState('');
     
+    function start(gamemode){
+        setHiddenClass('hidden');
+        props.setGamemode(gamemode);
+    }
+
     return(
         <>
         <Animation className={hiddenClass} />
         <section className={`titlescreen ${hiddenClass}`}>
             <h1>SwiftCalc</h1>
             <p>Complete mathematical operations as fast as you can</p>
-            <button onClick={() => setHiddenClass('hidden')}>Start</button>
+            <div style={{ display: 'flex' }}>
+                <button onClick={() => start('classic')}>Classic Mode</button>
+            </div>
         </section>
         </>
     )
