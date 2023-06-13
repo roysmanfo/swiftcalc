e990fe8117d7dbe803576474efdf85113f52d80f
RoysManfo <manforoys@gmail.com>
2023-06-13 11:55:55 +0200

    Added screen for selecting gamemode

--- src/componenets/TitleScreen.js
+++ src/componenets/TitleScreen.js
@@ -5,6 +5,24 @@ import '../css/components/titlescreen.css'
 
 export default function TitleScreen(props) {
     const [hiddenClass, setHiddenClass] = useState('');
+    const [modesAreVisible, setModesAreVisible] = useState(false);
+    const modes = [
+        {
+            "name": "Run",
+            "description": "Do 20 operation as fast as you can"
+        },
+        {
+            "name": "Bomb",
+            "description": "It is similar to run, but you only have 1min 30s"
+        },
+        {
+            "name": "Survival",
+            "description": "Do as many points as you can without failing"
+        },
+    ]
+
+
+
 
     function start(gamemode) {
         setHiddenClass('hidden');
@@ -13,8 +31,9 @@ export default function TitleScreen(props) {
 
     function GameModeButton(props) {
         return (
-            <button onClick={() => start(props.gamemode.toLowerCase())}>
+            <button style={{ gridArea: props.gamemode.toLowerCase() }} onClick={() => start(props.gamemode.toLowerCase())}>
                 {props.gamemode}
+                <small>{props.description}</small>
             </button>
         )
     }
@@ -24,11 +43,18 @@ export default function TitleScreen(props) {
         <>
             <Animation className={hiddenClass} />
             <section className={`titlescreen ${hiddenClass}`}>
-                <h1>SwiftCalc</h1>
-                <p>Complete mathematical operations as fast as you can</p>
-                <div style={{ display: 'flex', gap: '2rem' }}>
-                    <GameModeButton gamemode={'Classic'} />
-                    <GameModeButton gamemode={'Bomb'} />
+                <div className={modesAreVisible ? "title hidden" : "title"}>
+                    <h1 style={{ fontSize: '9em' }}>SwiftCalc</h1>
+                    <p>Complete mathematical operations as fast as you can</p>
+                    <button onClick={() => setModesAreVisible(true)}>Select a mode</button>
+                </div>
+                <div className={modesAreVisible ? 'gamemodes' : 'gamemodes hidden'}>
+                    <h1>Game modes</h1>
+                    <div className='modes'>
+                        {modes.map((m) => 
+                            <GameModeButton gamemode={m.name} description={m.description} key={m.name} />
+                        )}
+                    </div>
                 </div>
             </section>
         </>
