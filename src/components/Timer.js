7ace5b400272dac22d54b212e4a80b7308905b80
RoysManfo <manforoys@gmail.com>
2023-06-25 23:10:04 +0200

    Fixed timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import "../css/components/timer.css"
-
-
+import '../css/components/timer.css';
 
 function Timer() {
     const [seconds, setSeconds] = useState(0);
+    
+    // Start at -2 because on load the counter skips two numbers on render time (starts at 2:00 instead of 0:00)
+    const [min, setMin] = useState(-2); 
     const [time, setTime] = useState('0');
 
     useEffect(() => {
-
-        const min = parseInt(seconds / 60);
-        setTime(`${min}:${seconds < 10 ? "0" + seconds : seconds}`)
+        setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
 
         const intervalId = setInterval(() => {
-            setSeconds(prevSeconds => prevSeconds === 59 ? 0 : prevSeconds + 1);
+            setSeconds((prevSeconds) => (prevSeconds === 59 ? 0 : prevSeconds + 1));
         }, 1000);
 
         return () => {
             clearInterval(intervalId);
         };
+    }, [seconds, min]);
+
+    useEffect(() => {
+        if (seconds === 0) {
+            setMin((prevMin) => prevMin + 1);
+        }
     }, [seconds]);
 
-    return (
-        <div className='timer'>
-            {time}
-        </div>
-    );
+    return <div className="timer">{time}</div>;
 }
 
 export default Timer;
