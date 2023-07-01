87679e386931687c64dcfa18b431869207472022
RoysManfo <manforoys@gmail.com>
2023-07-01 17:46:06 +0200

    Stop timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../css/components/timer.css';
 
-function Timer() {
+function Timer(props) {
     const [seconds, setSeconds] = useState(0);
     
     // Start at -2 because on load the counter skips two numbers on render time (starts at 2:00 instead of 0:00)
@@ -9,8 +9,12 @@ function Timer() {
     const [time, setTime] = useState('0');
 
     useEffect(() => {
-        setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
+        if (props.stop) return;
 
+
+        setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
+        props.setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
+        
         const intervalId = setInterval(() => {
             setSeconds((prevSeconds) => (prevSeconds === 59 ? 0 : prevSeconds + 1));
         }, 1000);
@@ -18,7 +22,7 @@ function Timer() {
         return () => {
             clearInterval(intervalId);
         };
-    }, [seconds, min]);
+    }, [seconds, min, props]);
 
     useEffect(() => {
         if (seconds === 0) {
