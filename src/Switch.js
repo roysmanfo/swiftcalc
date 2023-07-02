54d409e0e4819aa4c9d1639e89fc04e3171acccd
RoysManfo <manforoys@gmail.com>
2023-07-02 08:46:27 +0200

    Redirect

--- src/Switch.js
+++ src/Switch.js
@@ -7,6 +7,6 @@ export default function Switch(){
     
     switch(gamemode){
         case 'run': return <Run />;
-        default: return <></>;
+        default: window.location.href = '/';
     }
 }
