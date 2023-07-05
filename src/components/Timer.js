276fbd4f25b5c2dcda3f3425e329509090c859be
RoysManfo <manforoys@gmail.com>
2023-07-05 02:52:25 +0200

    Solved problem with timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../css/components/timer.css';
 
 function Timer(props) {
     const [seconds, setSeconds] = useState(0);
-    
-    // Start at -2 because on load the counter skips two numbers on render time (starts at 2:00 instead of 0:00)
-    const [min, setMin] = useState(-2); 
+    const [min, setMin] = useState(0);
     const [time, setTime] = useState('0');
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         if (props.stop) return;
 
-
-        setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
-        props.setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
-        
-        const intervalId = setInterval(() => {
-            setSeconds((prevSeconds) => (prevSeconds === 59 ? 0 : prevSeconds + 1));
+        intervalRef.current = setInterval(() => {
+            setSeconds((prevSeconds) => {
+                if (prevSeconds === 59) {
+                    setMin((prevMin) => prevMin + 1);
+                    return 0;
+                } else {
+                    return prevSeconds + 1;
+                }
+            });
         }, 1000);
 
         return () => {
-            clearInterval(intervalId);
+            clearInterval(intervalRef.current);
         };
-    }, [seconds, min, props]);
+    }, [props.stop]);
 
     useEffect(() => {
-        if (seconds === 0) {
-            setMin((prevMin) => prevMin + 1);
-        }
-    }, [seconds]);
+        setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
+        props.setTime(`${min}:${seconds < 10 ? '0' + seconds : seconds}`);
+    }, [seconds, min, props]);
 
     return <div className="timer">{time}</div>;
 }
