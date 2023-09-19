c9486f61107a4628de72fd6afc4e762752922a01
RoysManfo <manforoys@gmail.com>
2023-09-19 22:23:57 +0200

    Readabiliy and maintainability improvements

--- src/components/TitleScreen.js
+++ src/components/TitleScreen.js
@@ -1,73 +1,73 @@
 import { useState } from 'react';
-import Animation from './Animation';
 import { Link } from 'react-router-dom';
+import Animation from './Animation';
+import leftArrowImg from "../img/svg/left-arrow.svg";
+import '../css/components/titlescreen.css';
 
-import '../css/components/titlescreen.css'
-import leftArrowImg from "../img/svg/left-arrow.svg"
-
+const modes = [
+    {
+        name: "Run",
+        description: "Do 30 operations as fast as you can",
+    },
+    {
+        name: "Bomb",
+        description: "Do as many points as you can in 1 minute",
+    },
+    {
+        name: "Survival",
+        description: "Do as many points as you can with just 3 hearts",
+    },
+    {
+        name: "Hardcore",
+        description: "Like survival, but with just 1 heart",
+    },
+];
 
 export default function TitleScreen(props) {
     const [hiddenClass, setHiddenClass] = useState('');
     const [modesAreVisible, setModesAreVisible] = useState(false);
-    const modes = [
-        {
-            "name": "Run",
-            "description": "Do 30 operation as fast as you can"
-        },
-        {
-            "name": "Bomb",
-            "description": "Do as many points as you can in 1 minute"
-        },
-        {
-            "name": "Survival",
-            "description": "Do as many points as you can with just 3 hearts"
-        },
-        {
-            "name": "Hardcore",
-            "description": "Like survival, but with just 1 heart"
-        },
-    ]
 
-
-
-
-    function start() {
+    const start = () => {
         setHiddenClass('hidden');
-    }
+    };
 
-    function GameModeButton(props) {
-        return (
-            <Link to={'/mode/' + props.gamemode.toLowerCase()} style={{ gridArea: props.gamemode.toLowerCase() }}>
-                <button onClick={() => start()}>
-                    {props.gamemode}
-                    <small>{props.description}</small>
-                </button>
-            </Link>
-        )
-    }
-
+    const GameModeButton = ({ gamemode, description }) => (
+        <Link to={`/mode/${gamemode.toLowerCase()}`} style={{ gridArea: gamemode.toLowerCase() }}>
+            <button onClick={start}>
+                {gamemode}
+                <small>{description}</small>
+            </button>
+        </Link>
+    );
 
     return (
         <>
             <Animation className={hiddenClass} />
-            <button onClick={() => setModesAreVisible(false)} title='Back to the title' className="back-button" style={modesAreVisible ? {} : {opacity: 0, zIndex: -9999}}>
-                <img src={leftArrowImg} alt='Arrow pointing left'/>
+            <button
+                onClick={() => setModesAreVisible(false)}
+                title="Back to the title"
+                className="back-button"
+                style={modesAreVisible ? {} : { opacity: 0, zIndex: -9999 }}
+            >
+                <img src={leftArrowImg} alt="Arrow pointing left" />
             </button>
             <section className={`titlescreen ${hiddenClass}`}>
                 <div className={modesAreVisible ? "title hidden" : "title"}>
-                    <h1>SwiftCalc </h1>
+                    <h1>SwiftCalc</h1>
                     <p>Complete mathematical operations as fast as you can</p>
                     <button onClick={() => setModesAreVisible(true)}>Select a mode</button>
                 </div>
-                <div className={modesAreVisible ? 'gamemodes' : 'gamemodes hidden'}>
-                    <h1>Game modes</h1>
-                    <div className='modes'>
-                        {modes.map((m) => 
-                            <GameModeButton gamemode={m.name} description={m.description} key={m.name} />
-                        )}
+                {modesAreVisible && (
+                    <div className="gamemodes">
+                        <h1>Game modes</h1>
+                        <div className="modes">
+                            {modes.map((m) => (
+                                <GameModeButton gamemode={m.name} description={m.description} key={m.name} />
+                            ))}
+                        </div>
                     </div>
-                </div>
+                )}
             </section>
         </>
-    )
+    );
 }
